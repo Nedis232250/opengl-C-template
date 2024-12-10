4fd058ef3dfa83e7320751a30858f1051006817b
Nedis232250 <97570089+Nedis232250@users.noreply.github.com>
2024-12-10 18:56:00 -0500

    Create buffer.h

---
+++ buffer.h#pragma once #include <GL/buffer.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <GL/glew.h>
+#include <stdint.h>
+#include <iostream>
+
+template<typename T> class Buffer {
+private:
+    uint32_t buffer;
+
+public:
+	Buffer(uint32_t target, T* data, size_t size) {
+        glGenBuffers(1, &buffer);
+        glBindBuffer(target, buffer);
+        glBufferData(target, size, data, GL_STATIC_DRAW);
+    }
+
+    uint32_t get_buffer() {
+        return buffer;
+    }
+
+    void set_buffer(uint32_t target, T* data, size_t size) {
+        glGenBuffers(1, &buffer);
+        glBindBuffer(target, buffer);
+        glBufferData(target, size, data, GL_STATIC_DRAW);
+    }
+
+    ~Buffer() {
+        glDeleteBuffers(1, &buffer);
+    }
+};
